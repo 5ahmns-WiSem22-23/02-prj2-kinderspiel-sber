8cf7d164b65e1fde7ea0301b3bf9b009cf09ede6
Kuschlsoke <mail2bergersi@gmail.com>
2023-01-25 10:33:19 +0100

    working win condition

--- Assets/Scripts/AffenCreator.cs
+++ Assets/Scripts/AffenCreator.cs
@@ -11,33 +11,47 @@ public class AffenCreator : MonoBehaviour
     private Vector3 offsetX = new Vector3(0, 1.1f, 0);
     private Vector3 offsetY = new Vector3(1.1f, 0, 0);
 
-    GameObject[] winRed, winBlue, winYellow, winGreen;
-    
+    public GameObject[] winRed, winBlue, winYellow, winGreen;
+
+    public GameObject winTextRed, winTextBlue, winTextGreen, winTextYellow;
      
     private void Start()
+    {
+        winTextRed.SetActive(false);
+        winTextBlue.SetActive(false);
+        winTextGreen.SetActive(false);
+        winTextYellow.SetActive(false);
+    }
+    void Update()
     {
         winRed = GameObject.FindGameObjectsWithTag("Red");
         winBlue = GameObject.FindGameObjectsWithTag("Blue");
         winYellow = GameObject.FindGameObjectsWithTag("Yellow");
         winGreen = GameObject.FindGameObjectsWithTag("Green");
-    }
-    void Update()
-    {
+
         if (winRed.Length >= 4)
         {
             Debug.Log("Red Win");
+            winTextRed.SetActive(true);
+            Destroy(gameObject);
         }
         if (winBlue.Length >= 4)
         {
             Debug.Log("Blue Win");
+            winTextBlue.SetActive(true);
+            Destroy(gameObject);
         }
         if (winYellow.Length >= 4)
         {
             Debug.Log("Yellow Win");
+            winTextYellow.SetActive(true);
+            Destroy(gameObject);
         }
         if (winGreen.Length >= 4)
         {
             Debug.Log("Green Win");
+            winTextGreen.SetActive(true);
+            Destroy(gameObject);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
